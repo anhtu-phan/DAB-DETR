ee79dc46a667510d53230af6951a1093f284808e
anhtu95 <anhtu23795@gmail.com>
2023-01-11 10:44:20 +0100

    update dockerfile

--- scripts/run.sh
+++ scripts/run.sh
@@ -1,6 +1,12 @@
 #!/bin/bash
 
 NAME="dab_detr"
-PROJECT_NAME="visdrone"
+PROJECT_NAME="protector"
+NUM_CLASSES=8
+coco_path=/usr/src/datasets/$PROJECT_NAME
+
+cd models/dab_deformable_detr/ops
+python setup.py build install
+cd ../../..
 
 python -m torch.distributed.launch main.py -m $NAME --output_dir logs/$PROJECT_NAME/$NAME/ --batch_size 1 --epochs 200 --lr_drop 20 --coco_path ~/detr/datasets/$PROJECT_NAME --finetune_ignore class_embed --pretrain_model_path models/DAB_DETR/checkpoint.pth --wandb_name $NAME --wandb_project_name $PROJECT_NAME --num_classes $NUM_CLASSES
